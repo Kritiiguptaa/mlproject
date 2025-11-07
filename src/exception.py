979f6508485c69505218692c40bd4a9738f136f3
Kritiiii <kriti.gupta1905@gmail.com>
2025-11-07 18:22:58 +0530

    Add notebooks for EDA and model training

--- src/exception.py
+++ src/exception.py
@@ -2,6 +2,7 @@
 
 import sys
 # provides access to system-specific parameters and functions
+from logger import logging
 import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -21,6 +22,7 @@ class CustomExecption(Exception):
     def __str__(self):
         return self.error_message
 
+#its like int main() to check if code working
 # if __name__=="__main__":
 #     try:
 #         a=1/0
